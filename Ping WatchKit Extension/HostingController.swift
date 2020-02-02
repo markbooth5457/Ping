b27c601d65498e4dff6ebdbb1df9c5db3dd96e37
Mark Booth <mark@markandpol.com>
2020-02-02 09:11:02 +0000

    Initial Commit

---
+++ Ping WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Ping WatchKit Extension
+//
+//  Created by Mark Booth on 02/02/2020.
+//  Copyright © 2020 Mark Booth. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
